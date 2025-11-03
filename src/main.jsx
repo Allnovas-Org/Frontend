b29c3c31450cc2a268ab94552af652a2dba1775b
akinolapo <akinolapo@gmail.com>
2025-11-03 17:41:31 +0100

    hero section fixed design

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter as Router} from "react-router-dom";
+import "@fontsource/mali"; // defaults to weight 400
+import "@fontsource/mali/600.css"; // optional extra weights
+
 
 const rootElement = document.getElementById('root');
 
