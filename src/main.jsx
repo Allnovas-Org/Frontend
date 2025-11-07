1d86fe57f50cb899b017d4b467f6ee43ec6a4ac3
Jeffreyxdev <agabaenwerejeffrey@gmail.com>
2025-11-07 18:48:49 +0100

    added back fontsource

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter as Router} from "react-router-dom";
-// import "@fontsource/mali"; // defaults to weight 400
-// import "@fontsource/mali/600.css"; // optional extra weights
+import "@fontsource/mali"; // defaults to weight 400
+import "@fontsource/mali/600.css"; // optional extra weights
 
 
 const rootElement = document.getElementById('root');
