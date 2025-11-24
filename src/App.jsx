e34f643c62c2c0d3ee59de2e8da077485f66403d
Ahmad Muhammad <theahmadm3@gmail.com>
2025-11-24 17:25:55 +0100

    Remove unused Header component import from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import './index.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home.tsx'
 import { About } from './pages/about.tsx'
-import { Header } from './components/Header.jsx'
 import Navbar from './components/Navbar.jsx'
+
 function App() {
 
 
   return (
     <>
-      {/* <Header /> */}
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
