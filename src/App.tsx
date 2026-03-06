be0e03252fe57c425de6ff9438c06256bbceb07f
itolimaesther <itolimaesther@gmail.com>
2026-03-07 00:56:42 +0100

    fix path2

--- src/App.tsx
+++ src/App.tsx
@@ -46,12 +46,6 @@ import ProjectDetails from "./pages/projects/projectDetails";
 import OfferFlow from "./pages/projects/projectDetails/hire";
 
 
-// Client pages
-import Projects from "./pages/projects";
-import PostJob from "./pages/projects/Postjob";
-import ProjectDetails from "./pages/projects/ProjectDetails";
-import OfferFlow from "./pages/projects/projectDetails/hire";
-
 function App() {
 	return (
 		<Routes>
